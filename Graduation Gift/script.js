d7575f46aca983bba76efc5b73e159e85a285ac6
Trung Nam <60380159+orionbear@users.noreply.github.com>
2024-01-31 23:03:40 +0700

    init

--- Graduation Gift/script.js
+++ Graduation Gift/script.js
@@ -1,4 +1,14 @@
 function toggleCurtain() {
   const curtain = document.querySelector('.curtain');
   curtain.classList.add('opened-curstain');
+
+  const prize = document.querySelector('.prize');
+  prize.classList.add('fly-down');
 }
+
+
+function goto() {
+  window.location.reload();
+}
+
+
