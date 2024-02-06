93aa1369027b0e578b17ce4cbc22939984f6b757
Nam <namptbt@gmail.com>
2024-02-06 23:05:59 +0700

    change beat

--- Graduation Gift/script.js
+++ Graduation Gift/script.js
@@ -10,72 +10,105 @@ let singer = [
   "wres.png",
 ]
 var index = 0;
+var solanclick = 0;
+const curtain = document.querySelector('.curtain');
 
 function toggleCurtain() {
-  const curtain = document.querySelector('.curtain');
   curtain.classList.add('opened-curstain');
 
   const letter = document.querySelector('.letter');
-  letter.classList.add('fly-down');
+  letter.classList.add('fly-down'); 
 }
 
 
+const letter = document.querySelector('.letter');
 function goto() {
-  const letter = document.querySelector('.letter');
-  letter.classList.add("animation-fly-out")
-  
-  // const domainExpand = document.querySelector('.domain-expand');
-  // domainExpand.classList.remove('d-none');
-  // domainExpand.play();
+  solanclick = solanclick + 1;
+
+  letter.classList.add("animation-fly-out");
 
   setTimeout(function() {
     letter.classList.add("d-none")
 
-    const word = document.querySelector('.word');
     word.classList.remove("d-none")
     wordflick();
 
-    setTimeout(function() {
-      word.classList.add("d-none") 
-      
-    }, 19000);
+    if(solanclick <= 1){
+      setTimeout(function() {
+        word.classList.add("d-none") 
+        stage.classList.remove('d-none');
+        videoElement.play(); 
+      }, 19000);
+    }else{
+      setTimeout(function() {
+        word.classList.add("d-none");
+        curtain.classList.remove('opened-curstain');
+        setTimeout(function() {
+          window.location.reload();
+        }, 3000);
+      }, 27000);
+    }
+    
 
   }, 3000);
 }
 
 const videoElement = document.querySelector('.singer-song');
 const singerElement = document.querySelector('.singer');
+const stage  = document.querySelector('.stage');
+
+const word = document.querySelector('.word');
 
 function play() {
   videoElement.play();
 
   if (!videoElement.paused){
+    index++;
     changeVideo();
-  }
-
-  videoElement.addEventListener("ended", function() {
-    changeVideo();    
-  });
+  } 
 }
 
+videoElement.addEventListener("ended", function() {
+  index++;
+  changeVideo();    
+});
+
 function changeVideo(){
-  index;
   if(index < songNames.length){
     videoElement.src = `./assets/${songNames[index]}`;
     singerElement.src = `./assets/${singer[index]}`
     videoElement.play();
+  }else{
+    stage.classList.add("d-none")
+    letter.classList.remove("d-none")
+    letter.classList.remove('animation-fly-out'); 
+    letter.classList.add('fly-down'); 
+
   }
-  index++;
 }
 
 var words = [
-  'Khoan đã',
-  'Chờ tớ một tí',
-  'Tớ còn vài điều muốn nói với cậu', 
+  ':)))',
+  'Cái gì cũng phải từ từ',
+  'Tớ mời được vài người đến dự lễ tốt nghiệp cậu nè', 
   'Cậu đã sẵn sàng chưa', 
-  'Bây giờ mình bắt đầu nhé !!'
-],
+  'Okay giờ bắt đầu thôi!!'
+];
 
+
+var words2 = [
+  ':)) Ahihi',
+  'Bất ngờ chỉ tới đây thôi',
+  'Bằng thật thì cậu phải tự đi lấy rồi',
+  'Còn cái này cậu chỉ được xem thôi',
+  'Khi nào tụi mình có dịp gặp nhau thì bù lại bằng cái khác nhá',
+  'Tinh thần dẫ thấy thoải mái chưa',
+  'Chuẩn bị tốt nghiệp thôi nào',
+  '<3 <3 <3 <3',
+]
+
+  
+var wordflick = function () {
   i = 0,
   offset = 0,
   len = words.length,
@@ -83,7 +116,11 @@ var words = [
   skip_count = 0,
   skip_delay = 15,
   speed = 70;
-var wordflick = function () {
+
+  if(solanclick != 1){
+    words = words2;
+    len = words2.length
+  }
   setInterval(function () {
       if (forwards) {
           if (offset >= words[i].length) {
